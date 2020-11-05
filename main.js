f5dca8e69637b185e2479c8be48f4d600da9f4d7
meachamm92 <meacham.space@gmail.com>
2020-11-05 10:45:47 -0600

    solved and stretch goal 1-2 complete;

--- main.js
+++ main.js
@@ -1,38 +1,76 @@
-// You should NOT change the HTML or CSS in this project (at least until you reach
-// the bonus objectives). Focus on the JavaScript.
+// VARIABLES -------------------------------------
+const replaceAllButton = document.querySelector(".replace-all-button");
+const replaceOneButton = document.querySelector(".replace-one-button");
+const rowElements = document.querySelectorAll(".row");
+const caseSensitive = document.querySelector("#case-sensitive");
+let count = 0;
+let done = false;
+let buttonType = "";
 
-const findInput = document.querySelector(".find-input")
-const replaceInput = document.querySelector(".replace-input")
-const replaceAllButton = document.querySelector(".replace-all-button")
+// EVENT LISTENERS --------------------------------
+replaceAllButton.addEventListener("click", function () {
+  buttonType = "all";
+  findAndReplace();
+});
 
-// The following variable holds your OUTER ARRAY of row elements.
-// Later you will need an OUTER LOOP to loop over the individual elements within
-// this array.
-const rowElements = document.querySelectorAll(".row")
+replaceOneButton.addEventListener("click", function () {
+  findAndReplace();
+});
 
-// When you call the function belwo, it will get and return an INNER ARRAY
-// containing the cell elements for a given row.
-// Call this function from WITHIN your row elements loop. Then you will, in turn,
-// need to loop over the resulting cell elements. But where should this whole
-// NESTED LOOP go? Think through the user's experience: when should WHAT happen? 
-function getCellElements (currentRowElement) {
-    return currentRowElement.querySelectorAll(".cell")
-}
+// caseSensitive.addEventListener("change", function (e) {
+//   if (!caseSensitive.checked) caseSensitive.value = false;
+//   else caseSensitive.value = true;
+// });
 
+// FUNCTIONS --------------------------------------------
+const getCellElements = function (currentRowElement) {
+  return currentRowElement.querySelectorAll(".cell");
+};
 
-// YOUR CODE GOES HERE
+const replacer = function (obj, findInput, replaceInput) {
+  obj.innerHTML = obj.innerHTML.replace(findInput, replaceInput);
+  count++;
+};
 
+// const finder = function (obj, findInput, replaceInput) {
+//   if (!caseSensitive.value) {
+//     if (obj.innerHTML.toLower.includes(findInput.toLower())) {
+//       obj.innerHTML = obj.innerHTML.toLower();
+//       replacer(obj, findInput, replaceInput);
+//     }
+//   } else {
+//     if (obj.innerHTML.includes(findInput)) {
+//       replacer(obj, findInput, replaceInput);
+//     }
+//   }
+// };
 
-// One last thing: dedicate very careful attention to using variables and
-// naming them accurately.
-// And when you change the value you are assigning to a variable, don't
-// forget to consider changing the name to reflect the change you made! It
-// is very easy to get confused when you are working inside NESTED LOOPS.
-// The best of us do. And unnecessary confusion during the process of 
-// developing your code means wasted time.
-//
-// The time-cost of structuring and naming things well is FAR less than the
-// time-cost of ignoring the quality and readability of your code.
-//
-// You can, of course, remove any comments in this starter project once
-// you have read them, if you prefer.
+const showReplaceCount = function (count) {
+  let fieldElement = document.querySelector("fieldset");
+  fieldElement.innerHTML +=
+    count > 0
+      ? `<div class='green'>Times replaced: ${count}</div>`
+      : "<div class='red'>No matches found</div>";
+};
+
+const findAndReplace = function () {
+  const findInput = document.querySelector(".find-input").value;
+  const replaceInput = document.querySelector(".replace-input").value;
+  for (let rowIndex = 0; rowIndex < rowElements.length; rowIndex++) {
+    const currentRow = getCellElements(rowElements[rowIndex]);
+    for (let cellIndex = 0; cellIndex < currentRow.length; cellIndex++) {
+      const currentCell = currentRow[cellIndex];
+      if (buttonType === "all") {
+        while (currentCell.innerHTML.includes(findInput)) {
+          replacer(currentCell, findInput, replaceInput);
+        }
+      } else {
+        if (currentCell.innerHTML.includes(findInput) && !done) {
+          done = true;
+          replacer(currentCell, findInput, replaceInput);
+        }
+      }
+    }
+  }
+  showReplaceCount(count);
+};
